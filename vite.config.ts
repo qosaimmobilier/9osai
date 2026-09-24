fd55ccde6c3dbf7c988ce8c2924d7cb593eb7563
qosaimmobilier <kosailakar@gmail.com>
2026-09-24 15:16:19 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/9osai/',
   plugins: [react()],
   resolve: {
     alias: {
